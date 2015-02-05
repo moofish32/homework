4c624b2956fdc6ab9c574239d0a097537579dd4a
Michael Cowgill <moofish32@gmail.com>
2015-02-05 08:57:21 +0000

    First working

--- cube_hunter.exs
+++ cube_hunter.exs
@@ -4,11 +4,16 @@ defmodule CubeHunter do
   end
 
   def do_find_n_cubes(n_cubes_to_find, next_value, n_cubes_to_find, seen, max_length) do
-    "IM DONE"
+    IO.puts "IM DONE"
+    key = cube_and_sort_digits(next_value - 1)
+    seen[:"#{key}"]
   end
 
   def do_find_n_cubes(n_cubes_to_find, next_value, current_max, seen, max_length) do
     "IM adding a new number #{next_value} cubed"
+    [updated_seen, {count, num}] = update_seen_and_count(seen, next_value)
+    IO.puts inspect(updated_seen)
+    do_find_n_cubes(n_cubes_to_find, next_value + 1, max(count, current_max), updated_seen, max_length)
   end
 
   def cube_and_sort_digits(number) do
@@ -32,14 +37,14 @@ ExUnit.start
 defmodule PermuteTest do 
   use ExUnit.Case
 
-  test "I have reached the end when n = max count" do 
-    assert CubeHunter.do_find_n_cubes(5, 6987987, 5, %{}, 987) == "IM DONE"
-  end
+  # test "I have reached the end when n = max count" do 
+  #   assert CubeHunter.do_find_n_cubes(5, 6987987, 5, %{}, 987) == "IM DONE"
+  # end
 
-  test "I am adding a new value" do 
-    assert CubeHunter.do_find_n_cubes(5, 987987, 1, %{}, 987) == "IM adding a new number 987987 cubed"
-  end
-
+  # test "I am adding a new value" do 
+  #   assert CubeHunter.do_find_n_cubes(5, 987987, 1, %{}, 987) == "IM adding a new number 987987 cubed"
+  # end
+  #
   test "given 12 return 12^3 as 1278 as a string" do 
     assert CubeHunter.cube_and_sort_digits(12) == "1278"
   end
@@ -51,5 +56,11 @@ defmodule PermuteTest do
     IO.puts num
     IO.puts inspect(new_dict)
     assert count == 4
+  end
+
+  test "345" do 
+    a = CubeHunter.find_n_cubes(3, 1)
+    IO.puts inspect(a)
+    assert true == true
   end
 end
